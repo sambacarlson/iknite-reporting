b1b02ddcdafac7d7640375acaa321a506d1d9343
sambacarlson <sambacarlson@gmail.com>
2024-11-26 15:58:23 +0100

    added colors and fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { Route, Routes } from "react-router";
+import Home from "./pages/home/Home";
+import NotFound from "./pages/NotFound/NotFound";
+
 function App() {
-  return <div>home</div>;
+  return (
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#024CAA",
+        secondary: "#DBD3D3"
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        courier: ['Courier New', "Courier", "monospace"]
+      }
+    },
   },
   plugins: [],
 }
